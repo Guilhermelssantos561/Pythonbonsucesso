b27744b6299339dca2d470e747da55cac8497f96
Guilhermelssantos561 <guilherme001luissantos@gmail.com>
2026-04-15 21:48:12 -0300

    Add files via upload

---
+++ Aula 1504/Questao01.py
@@ -0,0 +1,5 @@
+#Entrada de Dados
+#01. Escreva um programa que pede o raio de um círculo, e em seguida exiba o perímetro e área do círculo.
+r = int(input('Digite o valor do raio: '))
+area = 3.14 * r**2
+print('O valor da area e: ',area)
